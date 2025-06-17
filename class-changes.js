eb60ae6da27d7ef428863c1c6ab645b1ade001a9
ellaascott <esco0009@student.monash.edu>
2025-06-17 17:22:31 +1000

    Add files via upload

---
+++ class-changes.js
@@ -0,0 +1,56 @@
+const classChanges = [
+  { time: 197, target: ".main-text", add: "text-gold" },
+  { time: 200, target: ".main-text", remove: "text-gold" },
+  { time: 62, target: ".mainpage", add: "active" },
+
+  { time: 62.3, target: ".mainpage", remove: "active" },
+  { time: 62.6, target: ".mainpage", add: "active" },
+  { time: 62.9, target: ".mainpage", remove: "active" },
+
+  { time: 150, target: ".mainpage", add: "active1" },
+  { time: 171, target: ".mainpage", remove: "active1" },
+  { time: 120, target: ".mainpage", add: "active2" },
+
+  { time: 132, target: ".mainpage", remove: "active2" },
+  { time: 296, target: ".mainpage", add: "active3" },
+  { time: 299, target: ".mainpage", remove: "active3" },
+
+  { time: 30, target: ".pop1", add: "active" },
+  { time: 32, target: ".pop1", remove: "active" },
+  { time: 60, target: ".pop2", add: "active" },
+  { time: 62, target: ".pop2", remove: "active" },
+
+  { time: 90, target: ".pop3", add: "active" },
+  { time: 92, target: ".pop3", remove: "active" },
+  { time: 120, target: ".pop4", add: "active" },
+  { time: 122, target: ".pop4", remove: "active" },
+
+  { time: 150, target: ".pop5", add: "active" },
+  { time: 152, target: ".pop5", remove: "active" },
+  { time: 180, target: ".pop6", add: "active" },
+  { time: 182, target: ".pop6", remove: "active" },
+
+  { time: 210, target: ".pop7", add: "active" },
+  { time: 212, target: ".pop7", remove: "active" },
+  { time: 240, target: ".pop8", add: "active" },
+  { time: 242, target: ".pop8", remove: "active" },
+
+  { time: 270, target: ".pop9", add: "active" },
+  { time: 272, target: ".pop9", remove: "active" },
+  { time: 300, target: ".pop10", add: "active" },
+  { time: 302, target: ".pop10", remove: "active" },
+
+  { time: 330, target: ".pop11", add: "active" },
+  { time: 332, target: ".pop11", remove: "active" },
+  { time: 360, target: ".pop12", add: "active" },
+  { time: 362, target: ".pop12", remove: "active" },
+
+  { time: 390, target: ".pop13", add: "active" },
+  { time: 392, target: ".pop13", remove: "active" },
+  { time: 420, target: ".pop14", add: "active" },
+  { time: 422, target: ".pop14", remove: "active" },
+
+
+
+
+];
